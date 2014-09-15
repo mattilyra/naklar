3f69f51382232a8f5580b316a91ca6235d1fcb0b
Matti Lyra <m.lyra@sussex.ac.uk>
2014-09-15 16:17:05 +0100

    Added module for gensim convenience methods

---
+++ naklar/gensim.py
@@ -0,0 +1,8 @@
+__author__ = 'Matti Lyra'
+
+
+def topics_to_html():
+    """Turns a gensim topic distribution into an iPython HTML table.
+    :return:
+    """
+    raise NotImplementedError('Not implemented yet')
